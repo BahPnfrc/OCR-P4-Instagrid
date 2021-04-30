3a760a6adaeb2474088b3f752b9ce4994ff6a5e7
Pierre-Alexandre Bah <pierrealexandre.bah@genapi.fr>
2021-04-30 18:42:38 +0200

    - MAJ : Ajout des class de ViewController

---
+++ Instagrid/Controller/DisplayViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DisplayViewController.swift
+//  Instagrid
+//
+//  Created by Pierre-Alexandre on 30/04/2021.
+//
+
+import UIKit
+
+class DisplayViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Instagrid/Controller/SwipeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeViewController.swift
+//  Instagrid
+//
+//  Created by Pierre-Alexandre on 30/04/2021.
+//
+
+import UIKit
+
+class SwipeViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
