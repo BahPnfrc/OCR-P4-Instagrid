781f35b7f52c911e5b3d21e362a6a3a80fac0c52
Pierre-Alexandre Bah <pierrealexandre.bah@genapi.fr>
2021-05-05 10:56:47 +0200

    - MAJ : Corrections mineures

---
+++ Instagrid/View/SettingViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SettingViewController.swift
+//  Instagrid
+//
+//  Created by Pierre-Alexandre on 30/04/2021.
+//
+
+import UIKit
+
+class SettingViewController: UIStackView {
+
+    @IBOutlet weak var firstPic: UIImageView!
+    @IBOutlet weak var firstButton: UIButton!
+    
+    @IBOutlet weak var secondPic: UIImageView!
+    @IBOutlet weak var secondbutton: UIButton!
+    
+    @IBOutlet weak var thirdPic: UIImageView!
+    @IBOutlet weak var thirdButton: UIButton!
+
+}
